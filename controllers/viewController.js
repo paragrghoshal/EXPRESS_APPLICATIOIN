cd9457013cc46916c88d796ee67c77a0a4c74e3c
paragrghoshal <parag.qapro@gmail.com>
2023-11-08 20:13:40 -0500

    change colore code needs to be implementes

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,14 +1,17 @@
 const SiteCollection = require('../models/siteModel');
 const catchAsync = require('../utils/catchAsync');
-exports.getOverview = catchAsync(async (req,res,next)=>{
+exports.getOverview = catchAsync(async (req, res, next) => {
     const tours = await SiteCollection.find();
-    res.status(200).render('overview',{
+    res.status(200).render('overview', {
         tours
     });
 });
 
-exports.getTour = (req,res)=>{
-    res.status(200).render('tour',{
-        title:'tour the tour'
+exports.getTour = catchAsync(async (req, res) => {
+    const singleSite = await SiteCollection.findById(req.params.id);
+    //const tours = await SiteCollection.find();
+//console.log(singleSite.artist_name);
+    res.status(200).render('tour', {
+        singleSite
     });
-}
+});
--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -3,3 +3,31 @@ module.exports = fn => {
         fn(req, res, next).catch(next);
     };
 };
+
+// module.exports = changeColor => {
+//     var computedStyle = getComputedStyle(document.body);
+//     var backgroundColor = computedStyle.backgroundColor;
+//     // var change = document.getElementById("text-content")
+//     // var sun = document.getElementById("sun");
+//     // var moon = document.getElementById("moon");
+  
+//     if (backgroundColor === "rgb(20, 20, 20)") {
+//       setTimeout(() => {
+//         // change.textContent = "dark";
+//         // sun.style.display = "none";
+//         // moon.style.display = "flex";
+//         document.body.style.color = "black";
+//         document.body.style.backgroundColor = "rgb(255, 255, 255)";
+//       }, 500);
+//     }
+  
+//     if (backgroundColor === "rgb(255, 255, 255)") {
+//       setTimeout(() => {
+//         // change.textContent = "light";
+//         // sun.style.display = "flex";
+//         // moon.style.display = "none";
+//         document.body.style.color = "white";
+//         document.body.style.backgroundColor = "rgb(20, 20, 20)";
+//       }, 500);
+//     }
+//   }
